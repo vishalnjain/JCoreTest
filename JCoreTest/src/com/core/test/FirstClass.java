a3035f619a9a693dc9050ee50953a85505439f5b
vishalnjain <vishalnjain@gmail.com>
2023-01-13 12:50:55 +0530

    some modification

--- JCoreTest/src/com/core/test/FirstClass.java
+++ JCoreTest/src/com/core/test/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello....");
-
+		System.out.println("Hello once again .... as i wanted to run it from jenkins build job as JCoreTest..");
 	}
 
 }
